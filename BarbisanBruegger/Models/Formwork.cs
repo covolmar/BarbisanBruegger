a203c10f3f1bdc190e24c305348586c0ed70815a
covolmar <marco.covolan@bluewin.ch>
2019-05-23 16:52:36 +0200

    Add Formworks Model and Controller

---
+++ BarbisanBruegger/Models/Formwork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BarbisanBruegger.Models
+{
+	public class Formwork
+	{
+		public int ID { get; set; }
+		public string Description { get; set; }
+		public int Number { get; set; }
+		public int PricePerMonth { get; set; }
+
+	}
+
+	public class FormworkDBContext: DbContext
+	{
+		public DbSet<Formwork> Movies { get; set; }
+	}
+}
