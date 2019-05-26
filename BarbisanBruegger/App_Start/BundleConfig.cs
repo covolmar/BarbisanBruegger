1cc917e8175b243b273b31dc13595dc1ca3b62c9
covolmar <marco.covolan@bluewin.ch>
2019-05-26 20:04:25 +0200

    New Startpage and scrolling-nav.js

--- BarbisanBruegger/App_Start/BundleConfig.cs
+++ BarbisanBruegger/App_Start/BundleConfig.cs
@@ -14,17 +14,24 @@ namespace BarbisanBruegger
 			bundles.Add( new ScriptBundle( "~/bundles/jqueryval" ).Include(
 						"~/Scripts/jquery.validate*" ) );
 
-			// Verwenden Sie die Entwicklungsversion von Modernizr zum Entwickeln und Erweitern Ihrer Kenntnisse. Wenn Sie dann
-			// bereit ist für die Produktion, verwenden Sie das Buildtool unter https://modernizr.com, um nur die benötigten Tests auszuwählen.
-			bundles.Add( new ScriptBundle( "~/bundles/modernizr" ).Include(
+            bundles.Add(new ScriptBundle("~/bundles/jquery-easing").Include(
+                        "~/Scripts/jquery.easing.{version}.js"));
+
+            // Verwenden Sie die Entwicklungsversion von Modernizr zum Entwickeln und Erweitern Ihrer Kenntnisse. Wenn Sie dann
+            // bereit ist für die Produktion, verwenden Sie das Buildtool unter https://modernizr.com, um nur die benötigten Tests auszuwählen.
+            bundles.Add( new ScriptBundle( "~/bundles/modernizr" ).Include(
 						"~/Scripts/modernizr-*" ) );
 
 			bundles.Add( new ScriptBundle( "~/bundles/bootstrap" ).Include(
-					  "~/Scripts/bootstrap.js" ) );
+                      "~/Scripts/scrolling-nav.js",
+                      "~/Scripts/bootstrap.js" ) );
 
-			bundles.Add( new StyleBundle( "~/Content/css" ).Include(
+            bundles.Add(new ScriptBundle("~/bundles/scrolling-nav").Include(
+                      "~/Scripts/scrolling-nav.js"));
+
+            bundles.Add( new StyleBundle( "~/Content/css" ).Include(
 					  "~/Content/bootstrap.css",
-					  "~/Content/site.css" ) );
+                      "~/Content/site.css" ) );
 		}
 	}
 }
